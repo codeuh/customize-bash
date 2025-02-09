2cbbce8fc09edad0651c28d037d33ad33398a975
codeuh <codeuh@gmail.com>
2025-02-08 22:59:30 -0600

    Build script now podman exec's into the container with a bash terminal

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-devcontainer --docker-path podman --workspace-folder "$(pwd)" --remove-existing-container true up
+CONTAINER_ID=$(devcontainer --docker-path podman --workspace-folder "$(pwd)" --remove-existing-container true up | grep -o '{.*}' | jq -r '.containerId')
+
+
+podman exec -it $CONTAINER_ID bash
