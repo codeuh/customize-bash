999c65e912bbcbc965263e767b15844c81cd8946
codeuh <codeuh@gmail.com>
2025-02-08 23:31:12 -0600

    Remove mount from build.sh.

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 CONTAINER_ID=$(devcontainer --docker-path podman \
   --workspace-folder "$(pwd)" \
   --remove-existing-container true \
-  --mount "type=bind,source=/home/codeuh/.gitconfig,target=/root/.gitconfig" \
   up \
   | grep -o '{.*}' | jq -r '.containerId')
 
