b313ec7fdf7d3dbe23896080b67f4a4df974968e
codeuh <codeuh@gmail.com>
2025-02-09 12:52:42 -0600

    Whitespace admustment. Don't pwd multiple times.

--- build.sh
+++ build.sh
@@ -4,13 +4,11 @@ CURRENT_DIR=$(pwd)
 
 EXISTING_CONTAINER_ID=$(podman ps --filter "label=devcontainer.local_folder=${CURRENT_DIR}" --format "{{.ID}}")
 EXISTING_IMAGE_ID=$(podman inspect --format "{{.Image}}" $EXISTING_CONTAINER_ID)
-
 echo "Existing Container ID: $EXISTING_CONTAINER_ID"
-
 echo "Existing Image ID: $EXISTING_IMAGE_ID"
 
 CONTAINER_ID=$(devcontainer --docker-path podman \
-  --workspace-folder "$(pwd)" \
+  --workspace-folder $CURRENT_DIR \
   --remove-existing-container true \
   --id-label "devcontainer.local_folder=$CURRENT_DIR" \
   up \
@@ -19,7 +17,6 @@ CONTAINER_ID=$(devcontainer --docker-path podman \
 echo "Container ID: $CONTAINER_ID"
 
 NEW_IMAGE_ID=$(podman inspect --format "{{.Image}}" $CONTAINER_ID)
-
 echo "New Image ID: $NEW_IMAGE_ID"
 
 if [[ -n "$EXISTING_IMAGE_ID" && "$EXISTING_IMAGE_ID" != "$NEW_IMAGE_ID" ]]; then
